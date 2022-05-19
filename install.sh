ef9e540ca5faf09e47405531f73693ff32f265c8
keith-gamble <keith.gamble@bwdesigngroup.com>
2022-05-18 18:30:46 -0700

    Modified the installer file to use bash but then switch to zsh

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Replace the current default theme with a new one in the .zshrc file
 theme_name=kgamble
 echo "Changing theme to $theme_name"
 sed -ie "s/^ZSH_THEME=.*/ZSH_THEME=\"$theme_name\"/" ~/.zshrc
 
-# # Download the new theme
+# Download the new theme
 echo "Downloading $theme_name theme from github"
 touch ~/.oh-my-zsh/themes/$theme_name.zsh-theme
 curl -fsSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/kgamble.zsh-theme > ~/.oh-my-zsh/themes/$theme_name.zsh-theme
 
+# Change to the zsh shell
+zsh
 # # Restart the shell
 echo "Restarting shell"
 source ~/.zshrc

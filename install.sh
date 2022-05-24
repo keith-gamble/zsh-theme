5ac4b721601f3428586a3a1a87c71dc765361e46
keith-gamble <keith.gamble@bwdesigngroup.com>
2022-05-24 14:24:03 -0700

    Modified the installation sccript to not fail silently

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 # Replace the current default theme with a new one in the .zshrc file
 echo "Updating the .zshrc file"
 theme_name=kgamble
-curl -fsSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/.zshrc > ~/.zshrc
+curl -sSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/.zshrc > ~/.zshrc
 
 # Download the new theme
 echo "Downloading $theme_name theme from github"
 touch ~/.oh-my-zsh/themes/$theme_name.zsh-theme
-curl -fsSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/kgamble.zsh-theme > ~/.oh-my-zsh/themes/$theme_name.zsh-theme
+curl -sSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/kgamble.zsh-theme > ~/.oh-my-zsh/themes/$theme_name.zsh-theme

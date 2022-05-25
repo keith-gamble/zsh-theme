b3ba94ebe217cfa01d8aeb1e4cdaa88cd9c3498c
keith-gamble <keith.gamble@bwdesigngroup.com>
2022-05-25 08:56:17 -0700

    Added the license file adn updated the install.sh to include the with-zshrc flag

--- .zshrc
+++ .zshrc
@@ -8,7 +8,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="kgamble"
+ZSH_THEME="keith-gamble"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -106,5 +106,3 @@ alias ignition-testing-container="docker run --rm -d -p 9088:8088 --name ignitio
 bindkey -e
 bindkey "^[[1;9C" forward-word
 bindkey "^[[1;9D" backward-word
-
-
--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
 
-# Replace the current default theme with a new one in the .zshrc file
-echo "Updating the .zshrc file"
-theme_name=kgamble
-curl -sSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/.zshrc > ~/.zshrc
+THEME_NAME=keith-gamble
+
+if [ "$1" = "--with-zshrc" ]; then
+    # Replace the current default theme with a new one in the .zshrc file
+    echo "Updating the .zshrc file"
+   
+    curl -sSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/.zshrc > ~/.zshrc
+fi
 
 # Download the new theme
-echo "Downloading $theme_name theme from github"
-touch ~/.oh-my-zsh/themes/$theme_name.zsh-theme
-curl -sSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/kgamble.zsh-theme > ~/.oh-my-zsh/themes/$theme_name.zsh-theme
+echo "Downloading $THEME_NAME theme from github"
+touch ~/.oh-my-zsh/themes/$THEME_NAME.zsh-theme
+curl -sSL https://raw.githubusercontent.com/keith-gamble/zsh-theme/master/$THEME_NAME.zsh-theme > ~/.oh-my-zsh/themes/$THEME_NAME.zsh-theme
